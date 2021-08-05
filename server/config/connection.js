e64ef573d10a9ac059a29160babbb78ea4fad113
Dan Ringenbach <danringenbach.art@gmail.com>
2021-08-04 22:31:03 -0400

    mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+
+
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+mongoose.connection.on('connected', () =>
+  console.log('Connected to MongoDB Endpoint')
+);
+
+mongoose.connection.on('error', (err) =>
+  console.log(`Mongoose default connection error: ${err}`)
+);
 
 module.exports = mongoose.connection;
