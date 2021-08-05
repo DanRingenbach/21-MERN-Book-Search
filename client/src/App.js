8eb803f7b758babf643152b59ddd4607fd7afa6f
Dan Ringenbach <danringenbach.art@gmail.com>
2021-08-04 23:15:19 -0400

    apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, ApolloClient } from '@apollo/client'
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
   request: operation => {
